06d5a9edd32aa524299190db275cff3c8c0ec6a9
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-06 18:35:19 +0200

    Fix #16: synchronize the clock on the second edge, serial initialization

--- arduino/scripts/sync-clock.py
+++ arduino/scripts/sync-clock.py
@@ -3,6 +3,7 @@
 # Synchronize Arduin-o-punch clock via serial port
 
 import serial
+import time
 from datetime import datetime
 
 serial_port = '/dev/ttyUSB0'
@@ -10,7 +11,7 @@ baud_rate = 9600
 
 
 def get_current_time():
-    """Get the current time in milliseconds since 8:00."""
+    """Get the current time of day in milliseconds"""
     now = datetime.now()
     time_origin = now.replace(hour=0, minute=0, second=0, microsecond=0)
     elapsed_ms = int((now - time_origin).total_seconds() * 1000)
@@ -20,17 +21,34 @@ def get_current_time():
 with serial.Serial(serial_port, baud_rate, timeout=1) as ser:
     # Get current time before interacting with Arduino
     start = get_current_time()
-    # Get Arduin-o-punch clock reading
-    ser.write(b'clock\r')
-    arduino_clock = int(ser.readline().decode().strip())
-    print(f"Before: clock={arduino_clock}")
+    for i in range(10):
+        try:
+            # Get Arduin-o-punch clock reading
+            ser.write(b'clock\r')
+            arduino_clock = int(ser.readline().decode().strip())
+            print(f"Before: clock={arduino_clock}")
+            break
+        except ValueError:
+            pass
     # Get current time again
     finish = get_current_time()
 
     latency = (finish - start) // 2
     clock_offset = start + latency - arduino_clock
 
+    # Aim at the edge of the second
+    time_left_ms = finish % 1000
+    target_time = finish - time_left_ms
+    time_left_ms -= latency
+    if time_left_ms < 0:
+        # Too little time has left, try better luck on the next second
+        time_left_ms += 1000
+        target_time += 1000
+
+    # Let's assume the sleep time will be accurate enough to return in time
+    time.sleep(0.001 * time_left_ms)
+
     # Update Arduino clock
-    ser.write(f'clock {finish + latency}\r'.encode())
+    ser.write(f'clock {target_time}\r'.encode())
     arduino_clock = int(ser.readline().decode().strip())
     print(f"After: clock={arduino_clock}")
