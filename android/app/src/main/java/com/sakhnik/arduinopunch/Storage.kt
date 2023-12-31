54c619810d08b657e5908e8bd94c91b66e4112b7
Anatolii Sakhnik <sakhnik@gmail.com>
2023-12-31 17:56:25 +0200

    Fix #21 Remove clock offset remnants

--- android/app/src/main/java/com/sakhnik/arduinopunch/Storage.kt
+++ android/app/src/main/java/com/sakhnik/arduinopunch/Storage.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.widget.EditText
 import androidx.activity.ComponentActivity
 import java.io.FileNotFoundException
-import java.io.OutputStreamWriter
 
 open class Storage(private val activity: ComponentActivity) {
 
@@ -12,7 +11,6 @@ open class Storage(private val activity: ComponentActivity) {
 
     companion object {
         private const val KEY_FILENAME = "key.txt"
-        private const val OFFSETS_FILENAME = "offsets.txt"
     }
 
     fun getKey(): ByteArray {
@@ -43,32 +41,5 @@ open class Storage(private val activity: ComponentActivity) {
             }
         }
         return getKey()
-    }
-
-    open fun restoreClockOffsets(offsets: OffsetsT) {
-        offsets.clear()
-        try {
-            activity.applicationContext.openFileInput(OFFSETS_FILENAME).bufferedReader().use {
-                it.forEachLine { line ->
-                    val parts = line.split("\\s+".toRegex())
-                    if (parts.size == 2) {
-                        val station = parts[0].toInt()
-                        val offset = parts[1].toLong()
-                        offsets[station] = offset
-                    }
-                }
-            }
-        } catch (_: FileNotFoundException) {
-        }
-    }
-
-    open fun storeClockOffsets(offsets: OffsetsT) {
-        activity.applicationContext.openFileOutput(OFFSETS_FILENAME, Context.MODE_PRIVATE).use {
-            OutputStreamWriter(it).use { osw ->
-                offsets.forEach { (station, offset) ->
-                    osw.write("$station $offset\n")
-                }
-            }
-        }
     }
 }
