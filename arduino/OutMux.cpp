f1a8b443df621ae03c7df0c9d722e8e28e7376d1
Anatolii Sakhnik <sakhnik@gmail.com>
2024-05-01 16:56:03 +0300

    Send to bluetooth independently from serial #28

--- arduino/OutMux.cpp
+++ arduino/OutMux.cpp
@@ -12,11 +12,10 @@ size_t OutMux::write(uint8_t ch)
 
 size_t OutMux::write(const uint8_t *buffer, size_t size)
 {
-    auto written = Serial.write(buffer, size);
-    // Duplicate the output to the client (bluetooth) if connected.
+    // If there's a bluetooth connection, forward everything there
     if (_client)
-        _client->Write(buffer, written);
-    return written;
+        _client->Write(buffer, size);
+    return Serial.write(buffer, size);
 }
 
 int OutMux::availableForWrite(void)
