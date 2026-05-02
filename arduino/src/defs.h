11adc1dd1b939624514837cd0585a1e0550392d2
Anatolii Sakhnik <sakhnik@gmail.com>
2026-05-02 21:52:19 +0300

    [pcb] Add DS3231

--- arduino/src/defs.h
+++ arduino/src/defs.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#define LED_CONFIRM_PIN  2
-#define BUZZER_PIN 3
-#define MOSFET_PIN 21
 #define WAKEUP_PIN 0
+#define BUZZER_PIN 1
+#define LED_CONFIRM_PIN  2
+#define MOSFET_PIN 3
 
-#define I2C_SDA 10
-#define I2C_SCL 20
+#define I2C_SCL 10
+#define I2C_SDA 20
