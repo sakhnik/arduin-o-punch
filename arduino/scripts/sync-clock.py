c3ef5fe142b85dffa627a8da34704bc5870616af
Anatolii Sakhnik <sakhnik@gmail.com>
2026-08-11 21:54:26 +0300

    Revert to USB_CDC

--- arduino/scripts/sync-clock.py
+++ arduino/scripts/sync-clock.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta, timezone
 import argparse
 from connection import Connection, SerialConnection, TcpConnection
 
-serial_port = '/dev/ttyUSB0'
+serial_port = '/dev/ttyACM0'
 baud_rate = 115200
 
 parser = argparse.ArgumentParser()
